cb61d9eed1923e934fff5065bab7ce945980f5bf
Thomas <31560900+techtastet@users.noreply.github.com>
2021-10-05 21:27:47 +0200

    execute with update

--- execute.sh
+++ execute.sh
@@ -1,8 +1,11 @@
+apt update && apt upgrade -y
+
 # If on linux and curl not detected, apt update and install curl
+
 if [ -f /etc/os-release ]; then
     if [ -z "$(which curl)" ]; then
         echo 'curl not detected, installing...'
-        apt update && apt upgrade -y && apt install curl -y
+        apt install curl -y
     fi
 fi
 
