4b4a22eae8062698f7638534c614ce1861e6f127
Thomas <31560900+techtastet@users.noreply.github.com>
2021-10-05 16:12:21 +0200

    test with predefined comand

--- optimize.sh
+++ optimize.sh
@@ -55,8 +55,8 @@ if [ -f $1 ]; then
     # Delete output file
     mkdir ./$2
     # Convert input file to output file
-    ffmpeg -i $1 -preset veryslow -r 30 -crf 20 -f image2 ./$2/%d.jpg
-    # ffmpeg -i video.mp4 -preset veryslow -r 30 -crf 20 -f image2 ./output/%d.jpg
+    # ffmpeg -i $1 -preset veryslow -r 30 -crf 20 -f image2 ./$2/%d.jpg
+    ffmpeg -i video.mp4 -preset veryslow -r 30 -crf 20 -f image2 ./output/%d.jpg
     zip -9 ./$2.zip ./$2/*
     rm -R ./$2
 else
