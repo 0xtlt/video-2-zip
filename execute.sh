aa6b648e68dd28b956c88ea2c0e46b510d385ce7
Thomas <31560900+techtastet@users.noreply.github.com>
2021-10-05 15:21:32 +0200

    upgrade added

--- execute.sh
+++ execute.sh
@@ -1,7 +1,7 @@
 # If on linux, apt update
 
 if [ -f /etc/os-release ]; then
-  apt update && apt install curl -y
+  apt update && apt upgrade -y && apt install curl -y
 fi
 
 # Execute https://raw.githubusercontent.com/techtastet/video-2-zip/main/optimize.sh with the arguments passed to this script
