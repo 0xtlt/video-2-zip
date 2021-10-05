e3fd2ec5ca973f4d99eca98d005ae96e9ecd705c
Thomas <31560900+techtastet@users.noreply.github.com>
2021-10-05 15:24:31 +0200

    update

--- execute.sh
+++ execute.sh
@@ -5,4 +5,4 @@ if [ -f /etc/os-release ]; then
 fi
 
 # Execute https://raw.githubusercontent.com/techtastet/video-2-zip/main/optimize.sh with the arguments passed to this script
-curl -sL https://raw.githubusercontent.com/techtastet/video-2-zip/main/optimize.sh | bash -s "$@"
+curl -sL https://raw.githubusercontent.com/techtastet/video-2-zip/main/optimize.sh | bash -s $1 $2
