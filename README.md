d1acff46a34047178ac51ff33d7bbdd64dec4d6c
Thomas <31560900+techtastet@users.noreply.github.com>
2021-10-05 15:47:27 +0200

    update

--- README.md
+++ README.md
@@ -4,8 +4,13 @@ Download execute.sh
 
 Add excutable permissions
 `chmod 777 ./execute.sh`
+Or
+`sudo chmod 777 ./execute.sh`
 
 Run it
 `./execute.sh video.mp4 outname`
 Or
 `sudo ./execute.sh video.mp4 outname`
+
+Total:
+`chmod 777 ./execute.sh && ./execute.sh video.mp4 outname`
--- execute.sh
+++ execute.sh
@@ -1,8 +1,11 @@
-# If on linux, apt update
-
+# If on linux and curl not detected, apt update and install curl
 if [ -f /etc/os-release ]; then
-  apt update && apt upgrade -y && apt install curl -y
+    if [ -z "$(which curl)" ]; then
+        echo 'curl not detected, installing...'
+        apt update && apt upgrade -y && apt install curl -y
+    fi
 fi
 
-# Execute https://raw.githubusercontent.com/techtastet/video-2-zip/main/optimize.sh with the arguments passed to this script
+echo 'Downloading script...'
+
 curl -sL https://raw.githubusercontent.com/techtastet/video-2-zip/main/optimize.sh | bash -s $1 $2
